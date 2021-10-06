2af90b3587b4233e48868e3ef8e208cf3982b6f4
dysco72 <jeffreyscp087@gmail.com>
2021-10-05 22:11:15 -0700

    This is the first commit.

--- TestGit2/src/csc131/gittest/DevOne.java
+++ TestGit2/src/csc131/gittest/DevOne.java
@@ -1,9 +1,8 @@
 package csc131.gittest;
 
 public class DevOne {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
 
-	public static void main(String[] args) {
-		System.out.println("Hello Git");
-	}
-
+    }
 }
